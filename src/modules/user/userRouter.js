b71fa1bd7c60c5ec3b738aa4fa645edcd4cef083
Sabbir <sabbir195323@gmail.com>
2026-02-26 06:01:24 +0600

    almost done this registration process

--- src/modules/user/userRouter.js
+++ src/modules/user/userRouter.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const createUser = require('./userController');
+const {createUser,loginUser} = require('./userController');
+const verifyToken = require('../middleware/verifyToken');
 const userRouter = express.Router();
 
 userRouter.post('/register', createUser);
+userRouter.post('/login', loginUser);
 
 module.exports = userRouter;
